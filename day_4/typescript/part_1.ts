90682e65a88e3a9c1ab6c7f6e7b86574fbbd80c9
Miguel Coelho <miguelcoelhodev@gmail.com>
2025-12-06 12:56:17 +0000

    day 4

---
+++ day_4/typescript/part_1.ts
@@ -0,0 +1,59 @@
+import { readFromFile } from "@ts/utils";
+
+export async function part_1() {
+  const file = await readFromFile("input.txt").text();
+  // const file = await readFromFile("test_input.txt").text();
+
+  const matrix: Array<Array<"." | "@" | "X">> = [];
+
+  for (const line of file.split("\n")) {
+    matrix.push([".", ...line, "."] as Array<"." | "@" | "X">);
+  }
+  matrix.unshift(new Array(matrix[0]?.length).fill("."));
+  matrix.push(new Array(matrix[0]?.length).fill("."));
+
+  const result = checkRollsOfPaperRemovable(matrix)();
+
+  console.log(`Result: ${result}`);
+}
+
+function checkRollsOfPaperRemovable(matrix: Array<Array<"." | "@" | "X">>) {
+  return function () {
+    let nrRemovableRolls = 0;
+    const removableRolls: Array<Array<number>> = [];
+    const checkIsRemovable = checkIsRemovableForMatrix(matrix);
+    for (let i = 1; i < matrix.length - 1; i++) {
+      for (let j = 1; j < matrix[0]!.length - 1; j++) {
+        if (matrix[i]![j] === "@" && checkIsRemovable(i, j)) {
+          nrRemovableRolls++;
+          removableRolls.push([i, j]);
+        }
+      }
+    }
+    for (const [x, y] of removableRolls) {
+      matrix[x!]![y!] = "X";
+    }
+
+    return nrRemovableRolls;
+  };
+}
+
+function checkIsRemovableForMatrix(matrix: Array<Array<"." | "@" | "X">>) {
+  return function (x: number, y: number) {
+    if (
+      [
+        matrix[x - 1]![y - 1],
+        matrix[x]![y - 1],
+        matrix[x + 1]![y - 1],
+        matrix[x - 1]![y],
+        matrix[x + 1]![y],
+        matrix[x - 1]![y + 1],
+        matrix[x]![y + 1],
+        matrix[x + 1]![y + 1],
+      ].filter((c) => c === "@").length < 4
+    ) {
+      return true;
+    }
+    return false;
+  };
+}
